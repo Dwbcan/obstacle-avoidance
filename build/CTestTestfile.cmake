a435dd47d2c1cfebc6442dd0f115d1fffe2ae49f
Dwbcan <dew.bhaumik8@gmail.com>
2023-04-21 15:46:11 -0400

    build: Deleted unecessary build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Dew Bhaumik/Desktop/obstacle-avoidance
-# Build directory: C:/Users/Dew Bhaumik/Desktop/obstacle-avoidance/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
